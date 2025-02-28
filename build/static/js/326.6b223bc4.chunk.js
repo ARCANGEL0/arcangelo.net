ea443e1033db5c21bb6bfb72b73f72d95796ec35
Henry <henry.arcangello@proton.me>
2025-02-28 19:31:15 +0000

    eawe

---
+++ build/static/js/326.6b223bc4.chunk.js
@@ -0,0 +1,85 @@
+"use strict";(self.webpackChunklna=self.webpackChunklna||[]).push([[326],{5224:(e,i,s)=>{s.d(i,{z:()=>d});const t=(0,s(1191).ZP)("button")`
+  background: ${e=>e.color||"#212738ff"};
+  color: ${e=>e.color?"#212738ff":"#fff"};
+  font-size: 1rem;
+  font-weight: 700;
+  width: 100%;
+  border: 1px solid #edf3f5;
+  border-radius: 4px;
+  padding: 13px 0;
+  cursor: pointer;
+  margin-top: 0.625rem;
+  max-width: 180px;
+  transition: all 0.3s ease-in-out;
+  box-shadow: 0 16px 30px rgb(23 31 114 / 20%);
+
+  &:hover,
+  &:active,
+  &:focus {
+    color: #fff;
+    border: 1px solid rgb(187, 0, 0);
+    background-color: rgb(187, 0, 0);
+  }
+`;var n=s(184);const d=e=>{let{color:i,fixedWidth:s,children:d,onClick:r,className:l}=e;return(0,n.jsx)(t,{color:i,className:l,fixedWidth:s,onClick:r,children:d})}},9510:(e,i,s)=>{s.r(i),s.d(i,{default:()=>v});var t=s(605),n=s(9603),d=s(4720),r=s(4581),l=s(7413),o=s(1191);const a=(0,o.ZP)("section")`
+  position: relative;
+  padding: 10rem 0 8rem;
+
+  @media only screen and (max-width: 1024px) {
+    padding: 4rem 0 4rem;
+  }
+`,c=(0,o.ZP)("p")`
+  margin: 1.5rem 0 2rem 0;
+`,m=(0,o.ZP)("div")`
+  position: relative;
+  max-width: 540px;
+
+  @media only screen and (max-width: 575px) {
+    padding-top: 4rem;
+  }
+`,h=(0,o.ZP)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+`,x=(0,o.ZP)("h6")`
+  font-size: 15px;
+  line-height: 1rem;
+  padding: 0.5rem 0;
+  text-transform: uppercase;
+  color: #000;
+  font-family: "Motiva Sans Light", sans-serif;
+`,p=(0,o.ZP)("p")`
+  font-size: 0.5rem;
+`;var g=s(184);const f=(0,d.Z)()((e=>{let{icon:i,title:s,content:d,section:o,t:f,width:j,height:w,imgId:u,rowClass:b,id:Z}=e;return(0,g.jsx)(a,{id:Z,children:(0,g.jsx)(l.pT,{direction:"left",children:(0,g.jsxs)(t.Z,{justify:"space-between",align:"middle",className:b,children:[(0,g.jsx)(n.Z,{lg:11,md:11,sm:12,xs:24,children:""===i?"":(0,g.jsx)(r.l,{className:"",imgId:u,src:i,width:j,height:w})}),(0,g.jsx)(n.Z,{lg:11,md:11,sm:11,xs:24,children:(0,g.jsxs)(m,{children:[(0,g.jsx)("h6",{children:f(s)}),(0,g.jsx)(c,{children:f(d)}),(0,g.jsx)(h,{children:(0,g.jsx)(t.Z,{justify:"space-between",children:"object"===typeof o&&o.map(((e,i)=>(0,g.jsxs)(n.Z,{className:"iconCol",span:11,children:[(0,g.jsx)(r.l,{className:"iconImgCol",imgId:"",src:e.icon,width:"60px",height:"60px"}),(0,g.jsx)(x,{className:"iconTitleCol",children:f(e.title)}),(0,g.jsx)(p,{className:"iconContent",children:f(e.content)})]},i)))})})]})})]})})})}));var j=s(5224);const w=(0,o.ZP)("section")`
+  position: relative;
+  padding: 10rem 0 8rem;
+
+  @media only screen and (max-width: 1024px) {
+    padding: 8rem 0 6rem;
+  }
+
+  @media only screen and (max-width: 768px) {
+    padding: 4rem 0 3rem;
+  }
+`,u=(0,o.ZP)("p")`
+  margin: 1.5rem 0 2rem 0;
+`,b=(0,o.ZP)("div")`
+  position: relative;
+  max-width: 540px;
+
+  @media only screen and (max-width: 575px) {
+    padding-bottom: 4rem;
+  }
+`,Z=(0,o.ZP)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+
+  @media screen and (min-width: 1024px) {
+    max-width: 80%;
+  }
+
+  button:last-child {
+    margin-left: 20px;
+  }
+`,y=(0,d.Z)()((e=>{let{title:i,content:s,button:d,icon:o,t:a,imgId:c,width:m,height:h,rowClass:x,id:p}=e;return(0,g.jsx)(w,{id:p,children:(0,g.jsx)(l.pT,{direction:"right",children:(0,g.jsxs)(t.Z,{justify:"space-between",align:"middle",className:x,children:[(0,g.jsx)(n.Z,{lg:11,md:11,sm:11,xs:24,children:(0,g.jsxs)(b,{children:[(0,g.jsx)("h6",{children:a(i)}),(0,g.jsx)(u,{children:a(s)}),(0,g.jsx)(Z,{children:"object"===typeof d&&d.map(((e,i)=>(0,g.jsx)(j.z,{className:"",color:e.color,fixedWidth:!0,onClick:()=>(e=>{document.getElementById(e).scrollIntoView({behavior:"smooth"})})("services"),children:a(e.title)},i)))})]})}),""===o?"":(0,g.jsx)(n.Z,{lg:11,md:11,sm:12,xs:24,children:(0,g.jsx)(r.l,{className:"",imgId:c,src:o,width:m,height:h})})]})})})})),v=e=>"left"===e.type?(0,g.jsx)(f,{...e}):"right"===e.type?(0,g.jsx)(y,{...e}):null}}]);
+//# sourceMappingURL=326.6b223bc4.chunk.js.map

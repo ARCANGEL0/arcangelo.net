0533ae0be2ef0b8fd682646d5110b470e815a0f5
Henry <henry.arcangello@proton.me>
2025-02-28 19:39:55 +0000

    51

---
+++ LNA/static/js/687.cf2d0dd8.chunk.js
@@ -0,0 +1,29 @@
+"use strict";(self.webpackChunklna=self.webpackChunklna||[]).push([[687],{3687:(e,t,r)=>{r.r(t),r.d(t,{default:()=>c});var a=r(4720),n=r(1191),s=r(184);const i=n.F4`
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
+`,o=n.ZP.a`
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  z-index: 1000;
+  background-color: transparent;
+  cursor: pointer;
+
+  img {
+    width: 90px;
+    height: 90px;
+    animation: ${i} 2s infinite;
+
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`,c=(0,a.Z)()((e=>{let{title:t,content:a,t:n}=e;return(0,s.jsx)(o,{href:"https://wa.me/351927252945",target:"_blank",rel:"noopener noreferrer",children:(0,s.jsx)("img",{className:"img",src:r(6449),id:""})})}))},6449:(e,t,r)=>{e.exports=r.p+"static/media/Zap.b30a630a02b2e881c186.png"}}]);
+//# sourceMappingURL=687.cf2d0dd8.chunk.js.map

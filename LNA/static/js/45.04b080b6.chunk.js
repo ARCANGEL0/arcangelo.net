ff096d836de4fbbb33e1c8fb1d7cff41c18de8f0
Henry <henry.arcangello@proton.me>
2025-02-28 19:38:52 +0000

    we

---
+++ LNA/static/js/45.04b080b6.chunk.js
@@ -0,0 +1,20 @@
+"use strict";(self.webpackChunklna=self.webpackChunklna||[]).push([[45],{4413:(i,e,s)=>{s.r(e),s.d(e,{default:()=>g});var a=s(605),c=s(9603),d=s(4720),t=s(7413),m=s(5717),r=(s(3037),s(8688),s(1191));const n=(0,r.ZP)("section")`
+  position: relative;
+  padding: 7.5rem 0 3rem;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+
+  @media screen and (max-width: 1024px) {
+    padding: 5.5rem 0 3rem;
+  }
+`,p=((0,r.ZP)("p")`
+  padding: 0.75rem 0 0.75rem;
+`,(0,r.ZP)("div")`
+  max-width: 570px;
+
+  @media only screen and (max-width: 768px) {
+    max-width: 100%;
+  }
+`);var x=s(184),l={dots:!0,infinite:!0,speed:500,slidesToShow:2};const g=(0,d.Z)()((i=>{let{title:e,t:d,id:r}=i;return(0,x.jsx)(n,{id:r,children:(0,x.jsx)(t.Mi,{direction:"up",children:(0,x.jsx)(a.Z,{justify:"center",align:"middle",children:(0,x.jsx)(p,{children:(0,x.jsxs)(c.Z,{lg:24,md:24,sm:24,xs:24,children:[(0,x.jsx)("h6",{children:d(e)}),(0,x.jsx)("br",{}),(0,x.jsxs)(m.Z,{...l,children:[(0,x.jsx)("img",{className:"img",src:s(231),id:""}),(0,x.jsx)("img",{className:"img",src:s(106),id:""}),(0,x.jsx)("img",{className:"img",src:s(4531),id:""}),(0,x.jsx)("img",{className:"img",src:s(5610),id:""}),(0,x.jsx)("img",{className:"img",src:s(330),id:""}),(0,x.jsx)("img",{className:"img",src:s(1262),id:""}),(0,x.jsx)("img",{className:"img",src:s(890),id:""}),(0,x.jsx)("img",{className:"img",src:s(4350),id:""})]})]})})})})})}))},106:(i,e,s)=>{i.exports=s.p+"static/media/advocacia.2d52d8cc1d8caecdcfa0.png"},330:(i,e,s)=>{i.exports=s.p+"static/media/beleza.e66fead2f97923859295.png"},231:(i,e,s)=>{i.exports=s.p+"static/media/confeitaria.a51153637a036ac6473c.png"},4531:(i,e,s)=>{i.exports=s.p+"static/media/icarus2.ddc8a6e5cb8aecd01edd.png"},5610:(i,e,s)=>{i.exports=s.p+"static/media/icarus3.5d30f1c4ac107bc5f3df.png"},1262:(i,e,s)=>{i.exports=s.p+"static/media/imobiliaria.d9209b6d91c2b85f6dbb.png"},890:(i,e,s)=>{i.exports=s.p+"static/media/psico1.fe165e0986494ca7cc84.png"},4350:(i,e,s)=>{i.exports=s.p+"static/media/psico2.35bae1d14c5ac327ce94.png"}}]);
+//# sourceMappingURL=45.04b080b6.chunk.js.map
